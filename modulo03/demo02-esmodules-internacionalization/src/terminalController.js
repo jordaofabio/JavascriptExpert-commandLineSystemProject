8052c0aa3068c711d023be6654e78e7dfc98a055
jordaofabio <fabiosjordao@gmail.com>
2021-05-21 07:48:24 -0300

    Mostrando na tabela os dados digitados pelo usuario

--- modulo03/demo02-esmodules-internacionalization/src/terminalController.js
+++ modulo03/demo02-esmodules-internacionalization/src/terminalController.js
@@ -24,11 +24,19 @@ export default class TerminalController {
         const data = database.map(item => new Person(item).formatted(language));
         const table = chalkTable(this.getTableOptions, data);
         this.print = console.draft(table);
+
+        this.data = data;
+    }
+
+    updateTable(item) {
+        this.data.push(item);
+        this.print(chalkTable(this.getTableOptions(), this.data));
     }
 
     question(msg = '') {
         return new Promise(resolve => this.terminal.question(msg, resolve));
     }
+
     closeTerminal() {
         this.terminal.close();
     }
